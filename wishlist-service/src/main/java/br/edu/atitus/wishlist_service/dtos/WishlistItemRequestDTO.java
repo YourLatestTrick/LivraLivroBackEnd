a0301671dd5c6ec411e77078f472f949e164e1c2
Pedro Souza <pedrooliveirasouzaleal@gmail.com>
2025-11-12 01:24:14 -0300

    Remoção 'Quantidade' Microservice Wishlist

--- wishlist-service/src/main/java/br/edu/atitus/wishlist_service/dtos/WishlistItemRequestDTO.java
+++ wishlist-service/src/main/java/br/edu/atitus/wishlist_service/dtos/WishlistItemRequestDTO.java
@@ -1,6 +1,5 @@
 package br.edu.atitus.wishlist_service.dtos;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
@@ -9,9 +8,6 @@ public class WishlistItemRequestDTO {
     @NotNull(message = "bookId cannot be null")
     private UUID bookId;
 
-    @Min(value = 1, message = "quantity must be greater than 0")
-    private int quantity;
-
     // Getters e Setters
     public UUID getBookId() {
         return bookId;
@@ -19,13 +15,5 @@ public class WishlistItemRequestDTO {
 
     public void setBookId(UUID bookId) {
         this.bookId = bookId;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
     }
 }
